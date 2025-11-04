0e9669dca56781caefd4d3731d2308c161eb837a
Sanyam2649 <sb.sanyambansal2003@gmail.com>
2025-11-04 10:22:07 +0530

    static site deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-    images: {
+  output: 'export',
+
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
     ],
+    unoptimized: true, 
   },
 };
 
